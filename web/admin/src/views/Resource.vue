558a52efba85f2335285562f575789c0128114d2
yejunhong <2270704254@qq.com>
2019-03-16 19:44:51 +0800

    .

--- web/admin/src/views/Resource.vue
+++ web/admin/src/views/Resource.vue
@@ -10,7 +10,7 @@
       <el-table-column label="操作" fixed="right" width="120">
         <template slot-scope="scope">
           <el-button type="text" size="small">下载</el-button>
-          <el-button type="text" size="small">编辑</el-button>
+          <el-button type="text" size="small" @click="edit">编辑</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -19,16 +19,54 @@
       layout="prev, pager, next"
       :total="1000">
     </el-pagination>
+
+    <el-dialog :title="title" :visible.sync="resourceDialog">
+      <el-tabs v-model="resourceActive">
+        <el-tab-pane label="资源基本信息" name="base"></el-tab-pane>
+        <el-tab-pane label="爬虫配置" name="config"></el-tab-pane>
+      </el-tabs>
+      <el-form :model="form" v-show="resourceActive == 'base'">
+        <el-form-item label="编号" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="资源名称" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="书籍列表地址" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="爬虫配置" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <codemirror v-model="code" v-show="resourceActive == 'config'"></codemirror>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="resourceDialog = false">取 消</el-button>
+        <el-button type="primary" @click="resourceDialog = false">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 import http from '@/lib/http'
+import { codemirror } from 'vue-codemirror-lite'
 export default {
   data() {
     return {
-      resource_list: []
+      resourceActive: "base",
+      resource_list: [],
+      resourceDialog: false,
+      formLabelWidth: "100px",
+      title: "",
+      form: {
+
+      },
+      code: ""
     }
+  },
+  components: {
+    codemirror
   },
   mounted(){
     this.GetResource(1)
@@ -37,11 +75,17 @@ export default {
     async GetResource(page){
       const res = await http.get('/cartoon/resource')
       this.resource_list = res.list
+    },
+    edit() {
+      this.resourceDialog = true
     }
   }
 }
 </script>
 <style>
+.el-dialog{
+  width: 90vw !important;
+}
 .header{
   height: 5vh;
   color: rgb(0, 0, 0);
