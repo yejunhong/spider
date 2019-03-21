1dcb252203eb49a908f88b462c75ff77147feaf7
yejunhong <2270704254@qq.com>
2019-03-21 21:27:11 +0800

    .

--- node/src/service.ts
+++ node/src/service.ts
@@ -1,6 +1,24 @@
- import grpc from 'grpc';
+ 
+const grpc = require('grpc');
 const DriveClass = require('./drive');
 const node_rpc = grpc.load(`${__dirname}/../grpc/drive.proto`).grpc;
+
+class GrpcServer {
+
+  public Run() {
+    const server = new grpc.Server();
+    server.addProtoService(node_rpc.browser.service, {
+      CrawlList: async (call, callback) => { await drive_.CrawlList(call, callback) },
+      CrawlChapter: async (call, callback) => { await drive_.CrawlChapter(call, callback) },
+      CrawlChapterContent: async (call, callback) => { await drive_.CrawlChapterContent(call, callback) },
+    })
+    server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure())
+    server.start()
+  }
+}
+
+const grpcService = new GrpcServer();
+grpcService.Run();
 
 (async function () {
   const drive_ = new DriveClass();
